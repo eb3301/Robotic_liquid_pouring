0c1df9519ac34b318d644d9471cfd863b65b91a7
edo <edoardo.barutta00@gmail.com>
2025-10-29 16:33:12 +0100

    mod offset

--- ws_thesis/src/sim_plan/sim_plan/path_planner_service.py
+++ ws_thesis/src/sim_plan/sim_plan/path_planner_service.py
@@ -351,7 +351,7 @@ def generate_sim(parameters, view=False, liq=True, debug=False, video=False, app
     if approach:
         liqpos = (parameters['pos_init_cont'][0]-0.01,parameters['pos_init_cont'][1],parameters['pos_init_cont'][2]+container_size[2]+liquid_height/2) 
     else:
-        liqpos = (parameters['pos_grip_ee'][0]-parameters['offset'][1],parameters['pos_grip_ee'][1]+parameters['offset'][0], parameters['pos_grip_ee'][2]+parameters['offset'][2]+container_size[2]+liquid_height/2)
+        liqpos = (parameters['pos_grip_ee'][0]+parameters['offset'][1],parameters['pos_grip_ee'][1]-parameters['offset'][0], parameters['pos_grip_ee'][2]+parameters['offset'][2]+container_size[2]+liquid_height/2)
 
     if liq:
         liquid = scene.add_entity(
