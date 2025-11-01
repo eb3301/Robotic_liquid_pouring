0669b1ca6b9cc2f3453969403fe24bc181294f63
edo <edoardo.barutta00@gmail.com>
2025-11-01 22:44:48 +0100

    sm

--- ws_thesis/src/sim_plan/sim_plan/path_planner_service_validation_no_liq.py
+++ ws_thesis/src/sim_plan/sim_plan/path_planner_service_validation_no_liq.py
@@ -1592,7 +1592,7 @@ class PathPlannerService(Node):
 
         N = 20                   # Numero di modelli simulati (iniziale)
         M = 5                    # Numero di traiettorie
-        delta = 0.5              # Threshold di successo
+        delta = 0              # Threshold di successo
         view=False
         liq=False
         record=False
