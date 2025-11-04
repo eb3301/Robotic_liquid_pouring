0827484c55b912d8e79db6b14f796e7c12961c27
edo <edoardo.barutta00@gmail.com>
2025-11-04 22:33:46 +0100

    updated shrinking factor

--- ws_thesis/src/belief_updater/belief_updater/belief_updater_service.py
+++ ws_thesis/src/belief_updater/belief_updater/belief_updater_service.py
@@ -143,13 +143,13 @@ class BeliefUpdater(Node):
             # Fattore shrinking
             # k = iterazione corrente, H = orizzonte previsto
             # f0 = valore iniziale, f_min = minimo da raggiungere dopo H iteraz
-            H=1000
+            H=500
             f0, f_min = 1.0, 0.0001
             tau = H / np.log(f0 / f_min)   # es: H=1000 => tau≈334
             factor = max(f_min, f0 * np.exp(-k / tau))
             # Re-heating per avere + esploraz 
-            boost_every, boost = 500, 1.4
-            factor = min(1.0, factor * boost) if k % boost_every == 0 else factor
+            #boost_every, boost = 500, 1.4
+            #factor = min(1.0, factor * boost) if k % boost_every == 0 else factor
 
 
             # Applica scaling
