bcc0060f82ebeff50d2b684334bcfba52b235d54
edo <edoardo.barutta00@gmail.com>
2025-11-10 20:54:20 +0100

    sm

--- ws_thesis/src/sim_plan/sim_plan/path_planner_service_validation_no_liq.py
+++ ws_thesis/src/sim_plan/sim_plan/path_planner_service_validation_no_liq.py
@@ -1134,7 +1134,7 @@ def compute_fake_reward(parameters, theta_f, num_wp):
     err_max_num_wp=100
 
     w_pos, w_cor = 2, 1
-    w_theta, w_num_wp = 5,5
+    w_theta, w_num_wp = 5, 5
 
     reward1 = w_pos * max(0,1-pos_err/pos_tol)
     reward2 = w_cor * max(0,1-cor_err/cor_tol)
@@ -1819,7 +1819,7 @@ class PathPlannerService(Node):
         time = np.linspace(0, (n_points - 1) * dt, n_points).tolist()
         #best_path["time"] = time
 
-        new_threshold = min(max(np.mean(best_scores), threshold+0.0001),0.98)
+        new_threshold = min(max(np.mean(best_scores)/2, threshold+0.0001),0.98)
 
         # Converti in formato compatibile con .yaml e salva
         best_path=self.to_builtin(best_path)
