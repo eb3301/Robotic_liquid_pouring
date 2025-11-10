99387c4d9a4678824743c824a79f59aeae0fbcb2
edo <edoardo.barutta00@gmail.com>
2025-11-10 21:11:09 +0100

    added jitter in update

--- ws_thesis/src/belief_updater/belief_updater/belief_updater_service.py
+++ ws_thesis/src/belief_updater/belief_updater/belief_updater_service.py
@@ -532,11 +532,13 @@ class BeliefUpdater(Node):
                 yaml.safe_dump(state_TS, f, sort_keys=False)
 
             # Add jitter to diversify plan params in case of collapse:
-            theta_new = float(x_next_theta[0]) + np.random.uniform(-1.0, 1.0)/k
-            num_wp_new = int(x_next_num_wp[0] + np.random.uniform(-10, 10))/k
-
-            theta_new = np.clip(theta_new, 80, 100)
-            num_wp_new = int(np.clip(num_wp_new, 300, 400))
+            theta_new=[]
+            num_wp_new=[]
+            for i in PATH_NUM:
+                theta_new.append(np.clip(float(x_next_theta[i]) + np.random.uniform(-1.0, 1.0)/k, 80, 100))
+                num_wp_new.append(np.clip(int(x_next_num_wp[i] + np.random.uniform(-10, 10))/k, 300, 400))
+            x_next_theta=theta_new
+            x_next_num_wp=num_wp_new
             
             # save plan params
             state_current_plan_params = {
