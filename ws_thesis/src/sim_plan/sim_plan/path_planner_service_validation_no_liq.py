47e03e374b6ea28a209b94b8f11eb347ed2cd5e3
edo <edoardo.barutta00@gmail.com>
2025-11-12 13:39:19 +0100

    sm

--- ws_thesis/src/sim_plan/sim_plan/path_planner_service_validation_no_liq.py
+++ ws_thesis/src/sim_plan/sim_plan/path_planner_service_validation_no_liq.py
@@ -2065,7 +2065,8 @@ class PathPlannerService(Node):
         # Simulazione
         init_sim()
         candidate_paths = []
-        scene, ur5e, becher, becher2, liquid, dt = generate_sim(parameters_set[0],view,liq=False) # genera l'ambiente di simulazione
+        #scene, ur5e, becher, becher2, liquid, dt = generate_sim(parameters_set[0],view,liq=False) # genera l'ambiente di simulazione
+        ur5e=[]
         for i in range(len(parameters_set)):
             parameters = parameters_set[i] # ottiene l'n-esimo dizionario di parametri
             print(f"Parameters of iteration {i}: {parameters}")
