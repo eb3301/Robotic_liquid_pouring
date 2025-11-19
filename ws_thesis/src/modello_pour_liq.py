9c2110710ee51523aa560bedbd89059d6eec7f62
edo <edoardo.barutta00@gmail.com>
2025-11-19 09:59:59 +0100

    working pouring model

--- ws_thesis/src/modello_pour_liq.py
+++ ws_thesis/src/modello_pour_liq.py
@@ -10,9 +10,13 @@ def get_d(y: float, c: float, theta: float,R: float)->float:
     la distanza dal fondo, l'angolo d'inclinazione e il raggio
     """
     if np.isclose(np.tan(theta), 0.0): # Pelo libero quasi orizzontale
-        d = -c
+        if y < c:
+            return -R  # d <= -R → A = pi*R^2 (pieno)
+        else:
+            return R   # d >=  R → A = 0 (vuoto)
     else:
-        d=np.clip((y -c) /np.tan(theta), -R, R)
+        #d=np.clip((y - c) /np.tan(theta), -R, R)
+        d=(y - c) /np.tan(theta)
     return d
 
 def area(d: float, R: float) -> float:
@@ -28,22 +32,31 @@ def area(d: float, R: float) -> float:
     Output:
     A_liq = area di una slice di liquido
     """
-
     if R <= 0:
         return 0.0
     
-    # Limiti cilindro
-    if d <= -R:
-        return np.pi*R*R
-    if d >= R:
-        return 0.0
-    
-    A_sett = 0.5 * 2 * R**2 * np.arccos(d/R)
-    A_triang = 2 * 0.5 * np.sqrt(R**2 - d**2) * d
-    A_segm = A_sett - A_triang
+    # # Limiti cilindro
+    # if d <= -R:
+    #     A_liq = np.pi*R*R
+    # elif d >= R:
+    #     A_liq = 0.0
+    # else: 
+    #     A_sett = 0.5 * 2 * R**2 * np.arccos(d/R)
+    #     A_triang = 2 * 0.5 * np.sqrt(R**2 - d**2) * d
+    #     A_segm = A_sett - A_triang
+    #     A_liq = R**2*np.pi - A_segm
 
-    A_liq = R**2*np.pi - A_segm
-    
+    # profondità immersa
+    h = R - d
+
+    # limiti fisici
+    if h <= 0:
+        A_liq = 0.0
+    elif h >= 2*R:
+        A_liq = np.pi * R * R
+    else:
+        A_liq = (R*R) * np.arccos((R - h) / R) - (R - h) * np.sqrt(2*R*h - h*h)
+    #print(A_liq)
     return A_liq
 
 def volume(H: float, R: float, c: float, theta: float, n: int=1000)->float:
@@ -59,6 +72,7 @@ def volume(H: float, R: float, c: float, theta: float, n: int=1000)->float:
     A0 = area(d0, R)
     df = get_d(b, c, theta, R)
     Af = area(df, R)
+    #print(f"A0: {A0}, Af: {Af}")
     s = 0.5 * (A0 + Af)
 
     for i in range(1, n):
@@ -66,12 +80,13 @@ def volume(H: float, R: float, c: float, theta: float, n: int=1000)->float:
         d_i = get_d(y_i, c, theta, R)
         A_i = area(d_i, R)
         s += A_i
+        #print(s)
     V = s * h
     
-    return s * h
+    return V
 
 def find_c(H: float, R: float, theta: float, V_target: float,
-           c_min: float=-10.0, c_max: float=10.0, n: int=500, tol: float=1e-6)-> float:
+           c_min: float=-1e7, c_max: float=1e7, n: int=500, tol: float=1e-10)-> float:
     """
     Trova il valore di c tale che il volume calcolato coincida con V_target.
     Bisezione, assumendo monotonicità di V(c) nell'intervallo.
@@ -85,7 +100,7 @@ def find_c(H: float, R: float, theta: float, V_target: float,
 
     if f_min * f_max > 0:
         raise ValueError(
-            f"Nessuna radice nell'intervallo iniziale. "
+            f"Nessuna radice nell'intervallo. "
             f"[c_min={c_min}, f_min={f_min}] "
             f"[c_max={c_max}, f_max={f_max}]"
         )
@@ -108,13 +123,24 @@ def find_c(H: float, R: float, theta: float, V_target: float,
 def get_h_spill(c: float, theta: float, H: float, R: float) -> float:
     d_h = get_d(H,c,theta,R)
     h_spill = R - np.abs(d_h)
-
     return max(h_spill, 0.0)
+
+def _get_h_spill(c: float, theta: float, H: float, R: float) -> float:
+    # quota del pelo libero ai due bordi
+    z_plus  = c + R * np.tan(theta)
+    z_minus = c - R * np.tan(theta)
+
+    # quanto il pelo libero supera il bordo superiore (z = H)
+    h1 = z_plus  - H
+    h2 = z_minus - H
+
+    h_spill = min(max(h1, h2, 0.0),magic_number*R)
+    return h_spill
 
 def calc_v_trasc(trj):
     return 0
 
-def reward_pouring(num_waypoints: int, theta_f: float, vol_target: float, parameters: dict)->int:
+def reward_pouring(num_waypoints: int, theta_f: float, vol_target: float, parameters: dict, debug: bool)->int:
     # Parameters
     H = 9.5 * 1e-2
     R = 3 * 1e-2
@@ -122,7 +148,9 @@ def reward_pouring(num_waypoints: int, theta_f: float, vol_target: float, parame
     Cd = 0.6
     g = 9.81
     
-    plotter = FreeSurfacePlotter(width=2*R, height=H)
+    print("Simulating pouring")
+    if debug:
+        plotter = FreeSurfacePlotter(width=2*R, height=H)
 
     V_init = parameters["vol_init"] * 1e-6 if parameters["vol_init"]>1 else parameters["vol_init"]
     V_target = vol_target * 1e-6 if vol_target>1 else vol_target #40 * 1e-6 #parameters["vol_target"]
@@ -139,7 +167,7 @@ def reward_pouring(num_waypoints: int, theta_f: float, vol_target: float, parame
     z_min=0.967
     lip_height = parameters['pos_cont_goal'][2] + H +0.07
     quat_orizz = np.array([0.5,-0.5,0.5,-0.5])
-    pos4 = pos_cont
+    pos4 = pos_cont.copy()
     pos4[0]-=x_shift
     pos4[1] -= (R+0.03)
     pos4[2] += R
@@ -159,23 +187,22 @@ def reward_pouring(num_waypoints: int, theta_f: float, vol_target: float, parame
     V_poured=0
     V=V_init
     V_spilled = 0
-
+    c=0
     for i,theta in enumerate(theta_arr):
-       
-        c = find_c(H, R, theta, V)
-        
-        plotter.update(theta, c)
+        if V*1e6 > 0.5 and np.abs(c)<1 and theta<np.pi/2:
+            c = find_c(H, R, theta, V)
+            if debug:
+                plotter.update(theta, c)
 
         h_spill = get_h_spill(c, theta, H, R)
-        print(f"c: {c}, h: {h_spill}")
         L = 2 * np.arccos((R-h_spill)/R) * R
         
         # Update volumes:
-        Q = 2/3 * Cd * np.sqrt(2*g) * L * h_spill**1.5
-        V_i=Q*dt
-        V_poured = np.clip(V_poured + V_i, 0, V_init)
-        V = np.clip(V - V_i, 0, V_init)
-        print(f"[iter {i} - theta: {np.rad2deg(theta)}] - [Vol: {int(V*1e6)} Vol_poured: {int(V_poured*1e6)}]")
+        Q = 2/3 * Cd * np.sqrt(2*g) * L * h_spill**1.5 * 1.25 # 1.25 to account for not rect sect (exp param)
+        V_i = min(Q * dt, V)
+        if debug:
+            print(f"")
+            print(f"[iter {i} - theta: {np.rad2deg(theta)}] - [Vol: {(V*1e6):.3f} Vol_poured: {(V_poured*1e6):.3f} Vol_spilled: {(V_spilled*1e6):.3f}]")
         # Evaluate positions:                   # TODO aggiungere v_trascinamento e calc_v_trasc
         v = np.sqrt(2*g*h_spill)
         
@@ -192,16 +219,25 @@ def reward_pouring(num_waypoints: int, theta_f: float, vol_target: float, parame
 
         z_f = z_min+0.003
         y_f = y + v * np.sqrt(2*(z-z_f)/g)
-        x_f = x
+        x_f = x + 0.055
         p_liq = np.array([x_f, y_f, z_f])
+        
         err_pos=np.linalg.norm(p_liq[:-1]-pos_cont[:-1])
-        if err_pos >= R:
+        #print(f"p_liq: {p_liq}, p_cont: {pos_cont}")
+        #print(err_pos)
+        if err_pos >= 2*R:
+            V = np.clip(V - V_i, 0, V_init)
             V_spilled += V_i
-            V_poured -= V_i
-
+        else:
+            V = np.clip(V - V_i, 0, V_init)
+            V_poured += V_i
+    print("Simulation completed")
+    if not np.isclose(V+V_poured+V_spilled, V_init):
+        raise Warning("odiocrop")
+    print(f"[Vol: {(V*1e6):.3f} Vol_poured: {(V_poured*1e6):.3f} Vol_spilled: {(V_spilled*1e6):.3f}]")
     err_vol = np.abs(V_poured-V_target)
-    print(f"Volume poured: {V_poured}")
-    print(f"Volume spilled: {V_spilled}")
+    #print(f"Volume poured: {V_poured}")
+    #print(f"Volume spilled: {V_spilled}")
     if err_vol < tol_pour and V_spilled < tol_spill:
         return 1
     else:
@@ -219,11 +255,17 @@ class FreeSurfacePlotter:
         self.height = height
         
         # Vertici del rettangolo non ruotato (centrato in 0,0)
+        # self.rect_local = np.array([
+        #     [-width/2, -height/2],
+        #     [ width/2, -height/2],
+        #     [ width/2,  height/2],
+        #     [-width/2,  height/2]
+        # ])
         self.rect_local = np.array([
-            [-width/2, -height/2],
-            [ width/2, -height/2],
-            [ width/2,  height/2],
-            [-width/2,  height/2]
+            [-width/2, 0.0],
+            [ width/2, 0.0],
+            [ width/2,  height],
+            [-width/2,  height]
         ])
 
         # Figura
@@ -231,8 +273,11 @@ class FreeSurfacePlotter:
 
         # Limiti automatici se non forniti
         L = max(width, height)
+        # if xlim is None: xlim = (-L, L)
+        # if ylim is None: ylim = (-L, L)
+
         if xlim is None: xlim = (-L, L)
-        if ylim is None: ylim = (-L, L)
+        if ylim is None: ylim = (-0.1*L, 1.1*L)
 
         self.ax.set_xlim(*xlim)
         self.ax.set_ylim(*ylim)
@@ -285,7 +330,7 @@ def main():
                 "pos_init_ee":  (0.12394027698787038, 0.2665910764094347, 1.1638763741892955, -0.5837257860699608, 0.5925713099055904, -0.388471134978874, 0.3965017359886389),
                 "pos_grip_ee": (0.6509734785173125, 0.2847438888358813, 0.9764986855761588, -0.5000033337808922, 0.49998929956451965, -0.4999940109139501, 0.5000133554007884),
                 "offset": (0, 0.15, -0.02),
-                "dCoR": [0.0, 0.06, -0.004],
+                "dCoR": [0.0, 0.06, -0.004], 
                 "vol_init": 60.0, #2e-5, +-MAE
                 "densità": 998.0,
                 "viscosità": 0.001,
@@ -298,8 +343,10 @@ def main():
     theta_f=90
     num_waypoints=320  
     vol_target=40
-    reward=reward_pouring(num_waypoints, theta_f, vol_target, parameters)
-    
+    debug=False
+
+    reward=reward_pouring(num_waypoints, theta_f, vol_target, parameters, debug)
+    print(reward)
 if __name__ == '__main__':
     main()  
 
