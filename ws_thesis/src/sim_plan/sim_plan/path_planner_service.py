f9728055f6a02f7a3239574892f722a95b1f40a6
edo <edoardo.barutta00@gmail.com>
2025-11-19 12:00:03 +0100

    sm

--- ws_thesis/src/sim_plan/sim_plan/path_planner_service.py
+++ ws_thesis/src/sim_plan/sim_plan/path_planner_service.py
@@ -1156,8 +1156,9 @@ def plan_path_moveit(
     except Exception as e:
         raise RuntimeError(f"errore nella IK q1")
     q1=q2moveit(q1)
+    print(f"q1: {q1}")
     motion_client.move_to_joint(q1)
-    #print(f"q1: {q1}")
+    
 
     # pose_msg = PoseStamped()
     # pose_msg.header.frame_id = "world" # relative motion wrt tool0 frame
