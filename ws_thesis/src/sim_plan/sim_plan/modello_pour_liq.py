9232d2174b902c27d267d2b1d6227e22e6fb6961
Edoardo Barutta <barutta@faroni-vm-1.inside.dei.polimi.it>
2025-12-04 09:43:41 +0000

    sm

--- ws_thesis/src/sim_plan/sim_plan/modello_pour_liq.py
+++ ws_thesis/src/sim_plan/sim_plan/modello_pour_liq.py
@@ -3,6 +3,9 @@ from scipy.spatial.transform import Rotation as Rot
 import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
 from sim_plan.interp_data_pouring import get_k
+import mujoco
+import mujoco.viewer
+import yaml, os
 
 def get_d(y: float, c: float, theta: float,R: float)->float:
     """
@@ -294,6 +297,173 @@ def reward_pouring(num_waypoints: int, theta_f: float, vol_target: float, parame
     else:
         return 0
 
+def reward_pouring_rs(num_waypoints: int, theta_f: float, vol_target: float, parameters: dict, debug: bool = False)->int:
+    # Parameters
+    H = 9.5 * 1e-2
+    R = 3 * 1e-2
+    Cd = 0.6
+    g = 9.81
+    
+    dt=0.01
+    n_steps = int(num_waypoints/2)
+
+    print("Simulating pouring")
+    if debug:
+        plotter = FreeSurfacePlotter(width=2*R, height=H)
+
+    V_init = parameters["vol_init"] * 1e-6 if parameters["vol_init"]>1 else parameters["vol_init"]
+    V_target = vol_target * 1e-6 if vol_target>1 else vol_target #40 * 1e-6 #parameters["vol_target"]
+
+    if V_init < V_target:
+        V_target=V_init
+    
+    tol_pour = 0.15 * V_target
+    tol_spill = 0.1 * V_init
+        
+    theta_f = np.deg2rad(theta_f) if theta_f>2*np.pi else theta_f
+    theta_arr = np.concatenate((np.linspace(0, theta_f, n_steps), np.linspace(theta_f, 0.0, n_steps)))
+
+    k=get_k(np.rad2deg(theta_f), V_init*1e6, model = "linear")
+    pos_cont= np.array([parameters['pos_cont_goal'][0],parameters['pos_cont_goal'][1],parameters['pos_cont_goal'][2]])
+    
+    x_shift=0.15
+    z_min=0.967
+    lip_height = parameters['pos_cont_goal'][2] + H +0.07
+    quat_orizz = np.array([0.5,-0.5,0.5,-0.5])
+
+    pos4 = pos_cont.copy()
+    pos4[0]-=x_shift
+    pos4[1] -= R #(R+0.03)
+    pos4[2] += R
+    pos4[2]=max(pos4[2],z_min,lip_height)
+    quat4 = quat_orizz
+    CoR3D = np.array([
+        parameters['pos_cont_goal'][0] + parameters['dCoR'][0], # 0.0
+        parameters['pos_cont_goal'][1] - 0.005 + parameters['dCoR'][1], # - 0.01 
+        parameters['pos_cont_goal'][2] + parameters['dCoR'][2], # + 0.04
+    ])
+    p_tcp0 = pos4.copy()
+    R0 = Rot.from_quat(quat4) # matrice rot init
+    l = R0.inv().apply(CoR3D - p_tcp0) # offset tool0 --> CoR3D
+    lip_l = np.array([0.0, (R + 0.003), H])
+    axis_world  = np.array([1.0, 0.0, 0.0]) # asse x nel frame tool
+
+    V_poured=0
+    V=V_init
+    V_spilled = 0
+    c=0
+    for i,theta in enumerate(theta_arr):
+
+        if np.isnan(c) or np.isnan(theta):
+            print(f"NaN BEFORE find_c → iter={i}, theta={theta}, c={c}")
+            break
+
+        if V*1e6 > 0.5 and np.abs(c)<1 and theta<np.pi/2:
+            c = find_c(H, R, theta, V)
+            if np.isnan(c):
+                print(f"NaN in find_c → iter={i}, theta={theta}, V={V}, c={c}")
+                break
+            if debug:
+                plotter.update(theta, c)
+
+        h_spill = get_h_spill(c, theta, H, R)
+        L = 2 * np.arccos(np.clip( ((R - h_spill) / R), -1, 1)) * R
+        
+        # Update volumes:
+        Q = 2/3 * Cd * np.sqrt(2*g) * L * h_spill**1.5 * k # 1.25 to account for not rect sect (exp param)
+
+        h_spill = get_h_spill(c, theta, H, R)
+        if np.isnan(h_spill):
+            print(f"NaN in h_spill → iter={i}, theta={theta}, c={c}")
+            break
+
+        L = 2 * np.arccos(np.clip(((R - h_spill) / R), -1, 1)) * R
+        if np.isnan(L):
+            print(f"NaN in L → iter={i}, theta={theta}, h_spill={h_spill}")
+            break
+
+        Q = 2/3 * Cd * np.sqrt(2*g) * L * h_spill**1.5 * k
+        if np.isnan(Q):
+            print(f"NaN in Q → iter={i}, theta={theta}, h_spill={h_spill}, L={L}, k={k}")
+            break
+
+
+        V_i = min(Q * dt, V)
+        if debug:
+            print(f"")
+            print(f"[iter {i} - theta: {np.rad2deg(theta)}] - [Vol: {(V*1e6):.3f} Vol_poured: {(V_poured*1e6):.3f} Vol_spilled: {(V_spilled*1e6):.3f}]")
+        # Evaluate positions:                   # TODO aggiungere v_trascinamento e calc_v_trasc
+        v = np.sqrt( max((2*g*h_spill), 0.0) )
+        
+        R_theta = Rot.from_rotvec(theta * axis_world) * R0 # matrice rotazione lungo x
+        delta_pos=R_theta.apply(l)
+        p_tcp = CoR3D - delta_pos
+        p_tcp[2] = max(pos4[2],z_min, lip_height)
+
+        p_lip = p_tcp + R_theta.apply(lip_l)
+        # il movimento è nel piano Y-Z
+        x=p_lip[0]
+        y=p_lip[1]
+        z=p_lip[2]
+
+        z_f = z_min+0.003
+        y_f = y + np.clip(v * np.sqrt( max( (2*(z-z_f)/g), 0.0 ) ), -2*R,2*R)
+        x_f = x + 0.055
+        p_liq = np.array([x_f, y_f, z_f])
+        
+        #print(np.clip(v * np.sqrt( max( (2*(z-z_f)/g), 0.0 ) ), -R,R))
+        #print(f"p_liq: {p_liq}, p_cont: {pos_cont}")
+        
+        pos_cont_real = np.array([0.746, 0.961, 0.960])
+        # if i == n_steps:
+        #     print(f"pos cont: {pos_cont} - pos cont real: {pos_cont_real}")
+        #     print(f"pos tcp: {p_tcp} - {p_lip} - {p_liq}")
+        #     print(f"theta - {np.rad2deg(theta)}")
+
+        err_pos_x = p_liq[0]-pos_cont_real[0]
+        err_pos_y = p_liq[1]-pos_cont_real[1]
+        err_pos_z = p_liq[2]-pos_cont_real[2]
+        
+        # test=y-pos_cont_real[1]
+        # print(test, y, pos_cont_real[1])
+        # print(err_pos_x, err_pos_y, err_pos_z)
+        # if V_i >0:
+        #     print(f"{err_pos_x:.3f}, {err_pos_y:.3f}")
+        
+        if np.sqrt(err_pos_x**2 + err_pos_y**2) > 2*R:
+            V = np.clip(V - V_i, 0, V_init)
+            V_spilled += V_i
+        else:
+            V = np.clip(V - V_i, 0, V_init)
+            V_poured += V_i
+
+        # err_pos=np.linalg.norm(p_liq[:-1]-pos_cont[:-1])
+        # #print(err_pos)
+        # if err_pos >= 2*R:
+        #     V = np.clip(V - V_i, 0, V_init)
+        #     V_spilled += V_i
+        # else:
+        #     V = np.clip(V - V_i, 0, V_init)
+        #     V_poured += V_i
+
+        if np.isnan(V):
+            print(f"NaN in V → iter={i}, V_i={V_i}, V_spilled={V_spilled}, V_poured={V_poured}")
+            break
+
+    print("Simulation completed")
+    print(f"[Wp {num_waypoints} theta {np.rad2deg(theta_f)}]-[Vol: {(V*1e6):.3f} Vol_poured: {(V_poured*1e6):.3f} Vol_spilled: {(V_spilled*1e6):.3f}]")
+    if not np.isclose(V+V_poured+V_spilled, V_init):
+        raise Warning("Sum of volumes is not equal to the initial volume")
+    err_vol = np.abs(V_poured-V_target)
+    #print(f"Volume poured: {V_poured}")
+    #print(f"Volume spilled: {V_spilled}")
+    if err_vol < tol_pour and V_spilled < tol_spill:
+        # return np.clip(1 - err_vol / tol_pour, 0, 1)
+        # return np.clip(1 - V_spilled / tol_spill - 0.1 * err_vol / tol_pour, 0, 1)
+        return np.clip(1 - V_spilled / tol_spill, 0, 1)
+    else:
+        return 0
+
 class FreeSurfacePlotter:
     def __init__(self, width, height, xlim=None, ylim=None):
         """
@@ -373,7 +543,6 @@ class FreeSurfacePlotter:
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
 
-
 def main():
     parameters = {
                 "pos_init_cont": [0.8021465039268282, 0.2847160024669491, 0.9564999991059303],
@@ -391,12 +560,16 @@ def main():
                 "theta_f": 87, #+-15°
                 "num_wp": 320,
             }
+    
     theta_f=90
     num_waypoints=300  
     vol_target=40
     debug=False
 
-    reward=reward_pouring(num_waypoints, theta_f, vol_target, parameters, debug)
+    #reward=reward_pouring(num_waypoints, theta_f, vol_target, parameters, debug)
+    #print(reward)
+
+    reward = reward_pouring_rs(num_waypoints, theta_f, vol_target, parameters, debug)
     print(reward)
 
 if __name__ == '__main__':
